aca54fbaf07214d55c7f0299dfaed99f17c46a54
Adam Neary <adam@neary.com>
2014-02-05 09:37:56 -0500

    zsh changes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,10 @@
 alias reload!='. ~/.zshrc'
+
+# navigation
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias cdt="cd ~/Desktop"
+alias cdev="cd ~/Developer"
+
+alias psg="ps aux | grep "
